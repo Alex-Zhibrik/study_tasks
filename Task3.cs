98bba96f38549137d1e3265909e68b79da6c68e5
Alex-Zhibrik <vasyagrug1@gmail.com>
2024-11-21 21:54:57 +0300

    Create Task3.cs

---
+++ Task3.cs
@@ -0,0 +1,16 @@
+namespace Task3
+{
+    internal class Program
+    {
+        static double max;
+        static double a;
+        static double b;
+
+        static void Main(string[] args)
+        {
+            max = a > b ? a : b;
+        }
+
+       
+    }
+}
