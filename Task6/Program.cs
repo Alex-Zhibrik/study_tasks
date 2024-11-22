f2c0df2d50be9bd3ec341687dd3a15064b3fb936
Alex-Zhibrik <vasyagrug1@gmail.com>
2024-11-22 20:34:55 +0300

    update1

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello");
             Console.WriteLine("heeloo");
+            Console.WriteLine("dasdsad");
         }
     }
 }
